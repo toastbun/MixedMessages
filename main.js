d661f2c64bdf36f60009bffc65a4a16ddb57b763
Langley Maness <k.langleyguin@gmail.com>
2023-03-26 14:17:50 -0500

    Fix Error fortune to be more obvious

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const colors = ["red", "orange", "yellow", "green", "blue", "purple", "gold", "white", "silver", "black"]
 const numbers = [1, 7, 13, 26, 19, 11, 9, 3, 69]
-const fortunes = ["You are having a bad hair day.", "You will meet your soulmate.", "You will get a dog.", "The next game you play, you will win!", "Go back to sleep.", "Error."]
+const fortunes = ["You are having a bad hair day.", "You will meet your soulmate.", "You will get a dog.", "The next game you play, you will win!", "Go back to sleep.", "//Er//ro~r."]
 
 const randomizer = arr => {
     return Math.floor(Math.random() * arr.length)
