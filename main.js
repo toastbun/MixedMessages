2a2c041862a8e68eb8fce7e0ba0555286e54e5a4
Langley Maness <k.langleyguin@gmail.com>
2023-03-26 13:32:00 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const partOne = () => {
+    let randomLine = Math.floor(Math.random() * 9)
+    switch(randomLine) {
+        case 0:
+            return "red"
+            break;
+        case 1:
+            return "orange"
+            break;
+        case 2:
+            return "yellow"
+            break;
+        case 3:
+            return "green"
+            break;
+        case 4:
+            return "blue"
+            break;
+        case 5:
+            return "purple"
+            break;
+        case 6: 
+            return "silver"
+            break;
+        case 7: 
+            return "gold"
+            break;
+        case 8:
+            return "white"
+            break;
+        default:
+            return "error"
+            break
+    }
+
+}
+
+const partTwo = () => {
+    let randomLine = Math.floor(Math.random() * 9)
+    
+}
+
+const partThree = () => {
+    let randomLine = Math.floor(Math.random() * 9)
+    
+}
+
+
