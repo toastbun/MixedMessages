7eb439d3640105c1059c4463bf809fdaa533fe0e
Langley Maness <k.langleyguin@gmail.com>
2023-03-26 14:09:04 -0500

    Add randomizer

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ const colors = ["red", "orange", "yellow", "green", "blue", "purple", "gold", "w
 const numbers = [1, 7, 13, 26, 19, 11, 9, 3, 69]
 const fortunes = ["You are having a bad hair day.", "You will meet your soulmate.", "You will get a dog.", "The next game you play, you will win!", "Go back to sleep.", "Error."]
 
+const randomizer = arr => {
+    return Math.floor(Math.random() * arr.length)
+}
+
+let luckyColor = colors[randomizer(colors)]
+let luckyNumber = numbers[randomizer(numbers)]
+let luckyFortune = fortunes[randomizer(fortunes)]
 
